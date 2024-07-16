a8f186046d1bfc1ac089cfe85dc49d7d42a56fa6
okankukul <okukul@invio.com>
2024-07-16 09:10:26 +0300

    ISTAKIBI-6952 Android Eğitim ve Öğrenimi + Gerekli kütüphane implementation'larını yaptım.

--- app/src/main/java/com/okankkl/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/okankkl/movieapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.okankkl.movieapp
+package com.okankkl.movieapp.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.okankkl.movieapp.R
 
 class MainActivity : AppCompatActivity()
 {
