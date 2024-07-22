02079ded4e6b86fd8774f688066d7205ecd0c115
okankukul <okukul@invio.com>
2024-07-22 15:01:57 +0300

    ISTAKIBI-6952 Android Eğitim ve Öğrenimi + Viewall sayfasının tüm işlemleri bitti. + Search sayfasının yapısını da sayfalandırmaya göre yaptım.

--- app/build 2/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_okankkl_movieapp_MovieApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.okankkl.movieapp.MovieApplication"
-)
-public final class _com_okankkl_movieapp_MovieApplication {
-}
--- app/src/main/java/com/okankkl/movieapp/util/LayoutType.kt
+++
@@ -1,8 +0,0 @@
-package com.okankkl.movieapp.util
-
-enum class LayoutType
-{
-    Grid,
-    VerticalLinear,
-    HorizontalLinear,
-}
