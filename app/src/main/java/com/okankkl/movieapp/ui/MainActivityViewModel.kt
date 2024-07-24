12d25f34989b06868cec29f1564b4da728ca38d4
okankukul <okukul@invio.com>
2024-07-24 17:06:07 +0300

    ISTAKIBI-6952 Android Eğitim ve Öğrenimi + Uygulamanın son versiyonu

--- app/src/main/java/com/okankkl/movieapp/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/okankkl/movieapp/ui/MainActivityViewModel.kt
@@ -2,7 +2,6 @@ package com.okankkl.movieapp.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.okankkl.movieapp.domain.repository.MovieRepository
 import com.okankkl.movieapp.domain.repository.PreferenceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
